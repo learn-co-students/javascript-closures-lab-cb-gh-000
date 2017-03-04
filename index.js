dc7b17a5087b873040a744294051b389208197e3
Rafid Aslam <rafidteam@gmail.com>
2017-03-04 04:10:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const app = "I don't do much."
+function bumpCounter() {
+    var counter = 0;
+
+    function addBump() {
+        counter++;
+    }
+
+    function getBumps() {
+        return counter;
+    }
+
+    return {addBump, getBumps};
+}
+
+function createAnimal(animalType) {
+    function installDeadlyDevice(deadlyDevice) {
+        return {
+            animalType: animalType,
+            deadlyDevice: deadlyDevice
+        }
+    }
+
+    return installDeadlyDevice;
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
