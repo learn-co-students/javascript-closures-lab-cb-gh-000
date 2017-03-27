70b4f03670f3e8e52a2ec7c0b73c2c921bf9324e
Fadi George <fadii925@gmail.com>
2017-03-27 03:36:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const app = "I don't do much."
+const bumpCounter = () => {
+  let counter = 0;;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps,
+  };
+};
+
+const createAnimal = (animalType) => {
+  const createAnimal = (deadlyDevice) => ({ animalType, deadlyDevice });
+  return createAnimal;
+};
+
+const sharkCreator = createAnimal('Shark');
+const sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+const sharkWithFrickinCannon = sharkCreator('Cannon');
