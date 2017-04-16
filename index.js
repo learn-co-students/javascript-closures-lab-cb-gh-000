281424104a45bdb6cdedbea19757aa96b032fe49
Oscar Caceres <caceres.oscar7@gmail.com>
2017-04-16 13:58:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const app = "I don't do much."
+
+const bumpCounter = () =>{
+  let counter = 0;
+  const addBump = () => counter++
+  const getBumps = () => counter
+
+  return {addBump,getBumps}
+}
+const createAnimal = (animalType) =>(deadlyDevice) => ({ animalType, deadlyDevice})
+
+let sharkCreator = createAnimal('Shark');
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+let sharkWithFrickinCannon = sharkCreator('Cannon');
