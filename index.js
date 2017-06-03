7ef689dbd28778e1de6d28c965b20713a5df1c32
Robert Wayne <robertjwayne@gmail.com>
2017-06-03 19:47:44 -0400

    Create function bumpCounter()

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
