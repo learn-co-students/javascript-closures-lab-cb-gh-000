4bb2c18c39eaef985beb32df4aea955e6c487a2a
Robert Wayne <robertjwayne@gmail.com>
2017-06-03 20:36:54 -0400

    Create function createAnimal(animalType) and shark variables

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ function bumpCounter() {
     getBumps
   }
 }
+
+function createAnimal(animalType) {
+  function deadlyDevice(device) {
+    return {
+      animalType: animalType,
+      deadlyDevice: device
+    }
+  }
+
+  return deadlyDevice
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
