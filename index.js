28b95c8fd7f0cb95b5cf25ae31013ad228a656d1
Jade Anderson <enighter@gmail.com>
2017-07-25 01:38:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const app = "I don't do much."
+function bumpCounter(counter) {
+  var bump = 0;
+  function addBump() {
+    bump++;
+  }
+
+  function getBumps() {
+    return bump;
+  }
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function createDeadlyDevice(deadlyDevice) {
+    return {
+      animalType,
+      deadlyDevice
+    };
+  }
+  return createDeadlyDevice;
+}
+
+var sharkCreator = createAnimal("Shark");
+var sharkWithFrickinLaserbeam = createAnimal("Shark")("Laserbeam");
+var sharkWithFrickinCannon = createAnimal("Shark")("Cannon");
