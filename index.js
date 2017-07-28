6ac991ee69caf1c8555907d525620e35d6594bb8
Dominik Kurbiel <github email address>
2017-07-29 00:18:06 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much."
+function bumpCounter() {
+    const counter;
+    
+    function addBump(){
+        counter = counter + 1;
+    }
+    
+    function getBumps() {
+        return counter;
+    }
+    
+    return {
+        addBump,
+        getBumps
+    }
+}
