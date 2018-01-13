80e6e184eeef3b4584b130c485e892071146b05d
Brandon Villa <bornofos@gmail.com>
2018-01-13 03:42:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0
+
+  function addBump() {
+    counter = counter + 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  function innerFunc(deadlyDevice) {
+    return { animalType, deadlyDevice }
+  }
+
+  return innerFunc
+}
+
+var sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+
+var sharkWithFrickinCannon = sharkCreator('Cannon') 
