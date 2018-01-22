9bb975635fd96cadce47118a381ee893837f6a87
edwin rivera <waehake@gmail.com>
2018-01-22 00:56:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter += 1;
+    return counter;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps,
+  }
+}
+
+function createAnimal(animalType) {
+  function animalWithWeaponFunc(deadlyDevice) {
+    return {
+      animalType,
+      deadlyDevice,
+    }
+  }
+
+  return animalWithWeaponFunc;
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
