1faf092f51a3568e34ad1d6dcfad716f80f72ac5
Gary Thirkell <garythirkell@gmail.com>
2018-03-24 17:50:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  function addDeadlyDevice(deadlyDevice) {
+    return {
+      animalType,
+      deadlyDevice
+    }
+  }
+
+  return addDeadlyDevice;
+}
+
+let sharkCreator = createAnimal('Shark');
+
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+let sharkWithFrickinCannon = sharkCreator('Cannon');
