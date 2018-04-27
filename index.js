ea5d8cb88b3cce16a969f190debb40491ca32b57
Cameron Green <cgree78@gmail.com>
2018-04-27 23:32:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++
+  }
+  function getBumps() {
+    return counter;
+  }
+  return {addBump, getBumps}
+}
+
+function createAnimal(animalType) {
+  function attachDevice(deadlyDevice) {
+    return {animalType: animalType, deadlyDevice: deadlyDevice}
+
+  }
+  return attachDevice
+}
+
+const sharkCreator = createAnimal("Shark")
+
+const sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+const sharkWithFrickinCannon = sharkCreator("Cannon")
+
+console.log(sharkWithFrickinLaserbeam)
+console.log(sharkWithFrickinCannon)
