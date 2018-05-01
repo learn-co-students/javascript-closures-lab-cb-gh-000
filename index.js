bb868500e2ceb207531a0d77e510ac6edd89b345
Berry  Semexan <bsemexan@gmail.com>
2018-05-01 22:17:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var count = 0;
+  let addBump = () => (count += 1)
+  let getBumps = () => (count)
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType){
+  return (deadlyDevice) => ({ animalType, deadlyDevice })
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
