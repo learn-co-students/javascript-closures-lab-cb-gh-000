2d4f79c351d2880fc16d7593270327d522027aaf
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-23 23:57:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+function bumpCounter() {
+  counter = 0;
+  function addBump() {
+    counter = counter + 1;
+  }
+  
+  function getBumps() {
+    return counter;
+  }
+  
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  function f1(deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+  
+  return f1;
+}
+
+sharkCreator = createAnimal('Shark');
+sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+sharkWithFrickinCannon = sharkCreator('Cannon');
