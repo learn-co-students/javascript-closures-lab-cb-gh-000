75f619979e8c6050873a20061a1d6a9015b636af
IVAN V <ivanvalette@hotmail.com>
2019-04-26 18:56:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+function bumpCounter()
+{
+  var counter = 0;
+  function addBump()
+  {
+    counter++;
+  }
+
+  function getBumps()
+  {
+    return counter;
+  }
+
+  return{
+    addBump,
+    getBumps
+  }
+}
+
+
+
+function createAnimal(animalType)
+{
+  function ar(deadlyDevice)
+  {
+    var obj = {
+      "animalType" : animalType,
+      "deadlyDevice" : deadlyDevice
+    }
+    return obj
+  }
+
+  return ar;
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
+
+console.log(sharkWithFrickinCannon)
