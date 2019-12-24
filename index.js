31fe28f6e80e49c7d7ad162e1fbce33bf943f4a2
Saurav Singh <snipextt@gmail.com>
2019-12-24 15:36:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+function createAnimal(animalType){
+  const something = (deadlyDevice) =>{
+    return Object.assign({},{["animalType"]: animalType,["deadlyDevice"]: deadlyDevice})
+  }
+  return something
+}
+
+function bumpCounter(){
+  var counter=0
+  function addBump(){
+    counter+=1
+  }
+  function getBumps(){
+    return counter
+  }
+return {
+  addBump,
+  getBumps
+};
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
